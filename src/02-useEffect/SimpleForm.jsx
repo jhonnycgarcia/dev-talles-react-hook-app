5ebb3bc2996821dc2786a654a05cc509984c7058
Jhonny <jhonnycgarcia@gmail.com>
2024-11-04 22:55:51 -0500

    useEffect - SimpleForm

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: "John Doe",
+        email: "johndoe@example.com",
+    });
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    useEffect(() => {
+        
+    }, [])
+    
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr />
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="example@email.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
-import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
-    <CounterWithCustomHook />
+    {/* <CounterWithCustomHook /> */}
+    <SimpleForm />
   </StrictMode>,
 )
