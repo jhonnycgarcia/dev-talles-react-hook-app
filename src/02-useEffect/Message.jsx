a437775a4a2dd76b326da42e0f50f837bd7b3ca6
Jhonny <jhonnycgarcia@gmail.com>
2024-11-04 23:11:19 -0500

    useEffect - Precauciones

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,12 +1,20 @@
 import { useEffect } from "react"
 
+
+
 export const Message = () => {
 
     useEffect(() => {
-        console.log('Message mounted');
+
+        const onMouseMove = ({x, y}) => {
+            const coords = { x, y};
+            console.log(coords);
+        }
+
+        window.addEventListener('mousemove', onMouseMove);
 
         return () => {
-            console.log('Message unmounted');
+            window.removeEventListener('mousemove', onMouseMove);
         }
     }, [])
 
