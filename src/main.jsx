7515657d3ad015f5684ea99b15fb80642a0fd324
Jhonny <jhonnycgarcia@gmail.com>
2024-11-05 22:19:16 -0500

    Formularios con custom Hook

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,15 @@ import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <HooksApp /> */}
     {/* <CounterApp /> */}
     {/* <CounterWithCustomHook /> */}
-    <SimpleForm />
+    {/* <SimpleForm /> */}
+    <FormWithCustomHook />
   </StrictMode>,
 )
