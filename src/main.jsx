d6d1866a72c87645232df3a8d61064877e04306c
Jhonny <jhonnycgarcia@gmail.com>
2024-11-05 23:41:47 -0500

    Memo - Método de React

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,8 @@ import './index.css'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
     {/* <FormWithCustomHook /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <FocusScreen /> */}
-    <Layout />
+    {/* <Layout /> */}
+    <Memorize />
   </StrictMode>,
 )
