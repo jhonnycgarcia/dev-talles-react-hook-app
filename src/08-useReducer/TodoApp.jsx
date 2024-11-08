5bb87eeaec626747a67264020b88fee925028c82
Jhonny <jhonnycgarcia@gmail.com>
2024-11-07 22:26:52 -0500

    Creando el cascarón de la lista de Todo's

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -15,19 +15,58 @@ const initialState = [
 ];
 
 export const TodoApp = () => {
-    const [ todos, dispatchTodos ] = useReducer(todoReducer, initialState)
+    const [todos, dispatchTodos] = useReducer(todoReducer, initialState)
 
     return (
         <>
-            <h1>Todo App</h1>
+            <h1>
+                Todo App: 10, <small>pendientes: 2</small>
+            </h1>
             <hr />
-            <ul>
-                <li>item1</li>
-                <li>item2</li>
-                <li>item3</li>
-                <li>item4</li>
-                <li>item5</li>
-            </ul>
+
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        {
+                            todos.map((todo) => (
+                                <li 
+                                    key={todo.id}
+                                    className="list-group-item d-flex justify-content-between"
+                                >
+                                    <span className="align-self-center">
+                                        Item1
+                                    </span>
+                                    <button className="btn btn-outline-danger">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                        
+                    </ul>
+                </div>
+
+                <div className="col-5">
+                    <h4>Agregar Todos</h4>
+                    <hr />
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="¿Qué necesitas hacer?"
+                            className="form-control"
+                        />
+                        <button
+                            type="submit"
+                            className="btn btn-outline-primary mt-2"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+
+            </div>
+
+
         </>
     )
 }
